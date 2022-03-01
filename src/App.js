5809d2476fc3fc88b2b80af86333956032074056
Jake O'Toole <jake.otoole@nytimes.com>
2022-03-01 14:33:07 -0600

    Created a new component and imported it into App.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Peny" animal="dog" breed="Havaneese" />
-      <Pet name="Wren" animal="cat" breed="kitty" />
-      <Pet name="Riot" animal="cat" breed="kitty" />
+      <SearchParams />
     </div>
   );
 };
