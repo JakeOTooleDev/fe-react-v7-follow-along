7d12efe755e8b092542a78604f7385d56796b75b
Jake O'Toole <jake.otoole@nytimes.com>
2022-03-01 14:05:42 -0600

    Switched syntax to jsx.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Penny",
-      animal: "Dog",
-      breed: "Pit/Cattle",
-    }),
-    React.createElement(Pet, {
-      name: "Bowie",
-      animal: "Dog",
-      breed: "Cattle",
-    }),
-    React.createElement(Pet, {
-      name: "Riot",
-      animal: "Cat",
-      breed: "Cat?",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Peny" animal="dog" breed="Havaneese" />
+      <Pet name="Wren" animal="cat" breed="kitty" />
+      <Pet name="Riot" animal="cat" breed="kitty" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
