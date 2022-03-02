aed6c7a2aece8c76e3d3d61cc6bdddade5382e3b
Jake O'Toole <jake.otoole@nytimes.com>
2022-03-02 11:02:35 -0600

    Added router logic.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from "react/cjs/react.development";
+
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <h1>lol</h1>
+      <BrowserRouter>
+        <div>
+          <h1>Adopt Me!</h1>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
